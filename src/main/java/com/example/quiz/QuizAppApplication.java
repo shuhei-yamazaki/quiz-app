222985b58e42efbbff299c6197ddaa3b4b753e98
shuhei-yamazaki <s.s.54586404@gmail.com>
2023-09-19 10:51:04 +0900

    動作確認

--- src/main/java/com/example/quiz/QuizAppApplication.java
+++ src/main/java/com/example/quiz/QuizAppApplication.java
@@ -1,13 +1,99 @@
 package com.example.quiz;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.quiz.entity.Quiz;
+import com.example.quiz.repository.QuizRepository;
 
 @SpringBootApplication
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizAppApplication.class, args);
+		SpringApplication.run(QuizAppApplication.class, args).getBean(QuizAppApplication.class).execute();
 	}
 
+	@Autowired
+	QuizRepository repository;
+
+	/** 実行メソッド */
+	private void execute() {
+
+		// 登録処理
+		//		setUp();
+
+		// 全件取得
+		//		showList();
+
+		// １件取得
+		//		showOne();
+		
+		
+		deleteQuiz();
+	}
+
+	private void setUp() {
+
+		// エンティティ生成
+		Quiz quiz1 = new Quiz(null, "「Spring」はフレームワークですか？", true, "登録太郎");
+
+		// 登録実行
+		quiz1 = repository.save(quiz1);
+
+		// 登録確認
+		System.out.println("登録したデータは" + quiz1 + "です");
+
+		// エンティティ生成
+		Quiz quiz2 = new Quiz(null, "「Spring MVC」はバッチ処理機能を提供しますか？", false, "登録太郎");
+
+		// 登録実行
+		quiz2 = repository.save(quiz2);
+
+		// 登録確認
+		System.out.println("登録したデータは" + quiz2 + "です");
+
+	}
+
+	private void showList() {
+
+		System.out.println("--- 全件取得開始 ---");
+
+		// リポジトリを使用して全件取得を実施、結果を取得
+		Iterable<Quiz> quizzes = repository.findAll();
+
+		for (Quiz quiz : quizzes) {
+			System.out.println(quiz);
+		}
+
+		System.out.println("全件取得完了");
+	}
+
+	private void showOne() {
+
+		System.out.println("--- 1件取得開始 ---");
+
+		// リポジトリを使用して１件取得を実施、結果を取得(戻り値はOptional)
+		Optional<Quiz> quizOpt = repository.findById(1);
+
+		if (quizOpt.isPresent()) {
+			System.out.println(quizOpt);
+		} else {
+			System.out.println("該当する問題が存在しません");
+		}
+
+		System.out.println("--- １件取得完了 ---");
+	}
+
+	private void deleteQuiz() {
+
+		System.out.println("--- 削除処理開始 ---");
+		
+		// 削除実行
+		repository.deleteById(2);
+		
+		System.out.println("--- 削除処理終了 ---");
+	}
 }
--- src/main/java/com/example/quiz/entity/Quiz.java
+++ src/main/java/com/example/quiz/entity/Quiz.java
@@ -1,6 +1,7 @@
 package com.example.quiz.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "quiz")
 public class Quiz {
 	
 	@Id
